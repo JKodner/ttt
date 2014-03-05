dffb1b63ffd5c09ac0cfbdb02c10218d8d0d6ea9
Jacob Kodner <jake@kodner.net>
2014-03-05 06:55:30 -0800

    Added first aspect of computer gameplay: make a winning move

--- ttt.py
+++ ttt.py
@@ -1,7 +1,18 @@
+from random import choice
+from itertools import combinations
+from os import system
+
 """
 4 - 9 - 2
 3 - 5 - 7
 8 - 1 - 6
+
+Strategy: 
+1. Winning Move
+2. Block User Win,
+3. Corners
+4. Edges
+5. Center
 """
 
 def pboard():
@@ -11,6 +22,13 @@ def pboard():
 		if cnt % 3 == 0:
 			print "\n"
 		cnt += 1
+
+def check(p_lst):
+	token = False
+	for i in combinations(p_lst, 3):
+		if sum(i) == 15:
+			token = True
+	return token
 
 board = {}
 for i in range(1, 10):
@@ -22,17 +40,74 @@ inputs = {
 	7:8, 8:1, 9:6
 }
 
-keys = ["X", "O"]
-count = 1
+system('clear')
+print "You're X, and the Computer is O."
+if choice(["user", "computer"]) == "user":
+	keys = ["O", "X"]
+	print "You go First.\n"
+else:
+	keys = ["X", "O"]
+	print "The Computer Goes First\n"
+raw_input("Press Anything to Continue: ")
 
+count = 1
 user = []
 computer = []
+gamewin = False
 
 while count < 10:
+	system('clear')
+	
 	pboard()
 	sign = keys[count % 2]
-	place = int(raw_input("Turn %s-%s: " % (count, sign)))
-	board[inputs[place]] = sign
+	
+	if sign == "X":
+		turn = "User"
+		lst = user
+	else:
+		turn = "Computer"
+		lst = computer
+	
+	if turn == "User":	
+		# place = int(raw_input("Turn: %s, %s-%s: " % (turn, count, sign)))
+		# lst.append(inputs[place])
+		# board[inputs[place]] = sign
+		pass
+		
+
+	elif turn == "Computer":
+		go = raw_input("Go? ")
+		if go == "y":
+			winmove = []
+			token = False
+			for i in combinations(range(1, 10), 2):
+				diff = 15 - sum(i)
+				if 1 <= diff <= 8:
+					if diff not in i:
+						if board[diff] == " ":
+							if board[i[0]] == "O" and board[i[1]] == "O":
+								winmove.append(diff)
+								token = True
+								break
+			if token:
+				place = winmove[0]
+				lst.append(place)
+				board[place] = sign
+		else:
+			place = int(go)
+			lst.append(inputs[place])
+			board[inputs[place]] = sign
+
 	count += 1
+	if check(lst):
+		system('clear')
+		print "TIC TAC TOE"
+		winner = turn
+		gamewin = True
+		break
 
 pboard()
+if gamewin:
+	print "The %s Won!" % winner
+else:
+	print "It's a Draw!"
